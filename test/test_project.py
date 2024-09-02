bc53a09c77c70f66051ba3a6a344679224786755
pedrosalucas <54561127+pedrosalucas@users.noreply.github.com>
2024-09-02 15:19:46 -0300

    feat: Add tests for main function

--- test/test_project.py
+++ test/test_project.py
@@ -1,5 +1,7 @@
-from project import is_valid, month_to_integer, calculate_age, calculate_days
+from project import main, is_valid, month_to_integer, calculate_age, calculate_days
 from datetime import date
+from freezegun import freeze_time
+from io import StringIO
 import pytest
 
 
@@ -30,12 +32,21 @@ def test_month_to_integer_valueerror():
     with pytest.raises(ValueError):
         month_to_integer('febraury')
 
-
+@freeze_time("2023-08-21")
 def test_calculate_age():
     assert calculate_age("october", 20, 2000, date.today()) == 22
     assert calculate_age("nov", 0o2, 2001, date.today()) == 21
 
-
+@freeze_time("2023-08-21")
 def test_calculate_days():
     assert calculate_days("october", 20, date.today()) == 60
     assert calculate_days("nov", 0o2, date.today()) == 73
+
+@freeze_time("2023-08-21")
+def test_main_output(monkeypatch, capfd):
+    monkeypatch.setattr('sys.stdin', StringIO('October 20, 2000\n'))
+
+    main()
+    cature = capfd.readouterr()
+    assert "Your age as of August 21, 2023 is 22 years old." in cature.out
+    assert "60 days before your Birthday!" in cature.out
